c5685362dc88c1b6de5eba4ae182f45966ba6b89
Melaniegarcialapegna <mgarcial@fi.uba.ar>
2026-08-28 14:44:54 -0300

    Avance parte2_ejercicio4 Se mejora la implementaciion de las funciones recv/send_all para el cliente.

--- services/client/src/safe_socket/safe_socket.go
+++ services/client/src/safe_socket/safe_socket.go
@@ -1,22 +1,37 @@
 package safe_socket
 
-import "io"
-
-//TODO: Complete with a short-read/short-write tolerant implementation
+import (
+	"errors"
+	"io"
+)
 
 func SendAll(socket io.Writer, bytes []byte) error {
-	_, err := socket.Write(bytes)
-	if err != nil {
-		return err
+	totalSent := 0
+	for totalSent < len(bytes) { //loop continue until all bytes are sent
+		n, err := socket.Write(bytes[totalSent:])
+		if err != nil {
+			return err
+		}
+		if n == 0 {
+			return errors.New("connection closed after sending all data")
+		}
+		totalSent += n
 	}
 	return nil
 }
 
 func RecvAll(socket io.Reader, size int) ([]byte, error) {
 	buff := make([]byte, size)
-	n, err := socket.Read(buff)
-	if err != nil {
-		return nil, err
+	totalRead := 0
+	for totalRead < size { //loop continue until all bytes are read
+		n, err := socket.Read(buff[totalRead:])
+		if err != nil {
+			return nil, err
+		}
+		if n == 0 {
+			return nil, errors.New("connection closed after reading all data")
+		}
+		totalRead += n
 	}
-	return buff[:n], nil
+	return buff, nil
 }
