3713425d154d57ce5f69c2db333dbabc4d8f8f74
Melaniegarcialapegna <mgarcial@fi.uba.ar>
2026-08-28 15:15:17 -0300

    Terminar funcionalidad parte2_ejercicio4 Se mejora la implementacion de las funciones recev_all y send_all para el servidor.

--- services/client/src/safe_socket/safe_socket.go
+++ services/client/src/safe_socket/safe_socket.go
@@ -8,14 +8,14 @@ import (
 func SendAll(socket io.Writer, bytes []byte) error {
 	totalSent := 0
 	for totalSent < len(bytes) { //loop continue until all bytes are sent
-		n, err := socket.Write(bytes[totalSent:])
+		sizeSent, err := socket.Write(bytes[totalSent:])
 		if err != nil {
 			return err
 		}
-		if n == 0 {
-			return errors.New("connection closed after sending all data")
+		if sizeSent == 0 {
+			return errors.New("connection closed before sending all data")
 		}
-		totalSent += n
+		totalSent += sizeSent
 	}
 	return nil
 }
@@ -24,14 +24,14 @@ func RecvAll(socket io.Reader, size int) ([]byte, error) {
 	buff := make([]byte, size)
 	totalRead := 0
 	for totalRead < size { //loop continue until all bytes are read
-		n, err := socket.Read(buff[totalRead:])
+		sizeRead, err := socket.Read(buff[totalRead:])
 		if err != nil {
 			return nil, err
 		}
-		if n == 0 {
-			return nil, errors.New("connection closed after reading all data")
+		if sizeRead == 0 {
+			return nil, errors.New("connection closed before reading all data")
 		}
-		totalRead += n
+		totalRead += sizeRead
 	}
 	return buff, nil
 }
--- services/server/src/safe_socket/safe_socket.py
+++ services/server/src/safe_socket/safe_socket.py
@@ -1,11 +1,20 @@
 import socket
 
-# TODO: Complete with a short-read/short-write tolerant implementation
-
-
-def recv_all(socket: socket.socket, size):
-    return socket.recv(size)
-
 
 def send_all(socket: socket.socket, bytes):
-    return socket.send(bytes)
+    total_sent = 0
+    while total_sent < len(bytes): #loop continue until all bytes are sent
+        size_sent = socket.send(bytes[total_sent:])
+        if size_sent == 0: 
+            raise ConnectionError("Connection closed before sending all data")
+        total_sent += size_sent
+    return total_sent
+
+def recv_all(socket: socket.socket, size):
+    data = b""
+    while len(data) < size: #loop continue until all bytes are read
+        data_read = socket.recv(size-len(data))
+        if data_read == b"": 
+            raise ConnectionError("Connection closed before reading all data")
+        data += data_read
+    return data
