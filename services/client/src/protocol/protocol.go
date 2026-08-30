e25d34ee2f35f9edf1b35cebcbfc9712e8a01b67
Melaniegarcialapegna <mgarcial@fi.uba.ar>
2026-08-30 14:38:28 -0300

    Avance parte2_ejercicio5 Se agrega logica de envio de mensaje para avisar que se terminaron de enviar las apuestas.

--- services/client/src/protocol/protocol.go
+++ services/client/src/protocol/protocol.go
@@ -6,13 +6,15 @@ import (
 	"encoding/binary"
 	"io"
 
+	"github.com/7574-sistemas-distribuidos/tp-nivelador/src/logger"
 	"github.com/7574-sistemas-distribuidos/tp-nivelador/src/model"
 	"github.com/7574-sistemas-distribuidos/tp-nivelador/src/safe_socket"
 )
 
 // TODO: ver si poner ctes en otro lado
-const AMOUNT_BYTES_UINT32 = 4
+const AMOUNT_BYTES_UINT8 = 1
 const AMOUNT_BYTES_UINT16 = 2
+const AMOUNT_BYTES_UINT32 = 4
 
 // serializa apuesta, le agrega header y manda por socket
 func SendBet(socket io.Writer, bet model.Bet) error {
@@ -23,16 +25,18 @@ func SendBet(socket io.Writer, bet model.Bet) error {
 
 	//paso a siguiente capa
 	if err := safe_socket.SendAll(socket, betMessage); err != nil {
+		logger.Error("send-message", logger.Fail) //TODO: ver que mas agregarle al log y si es correcto ponerlo aca o con la capa de arriba basta xd
 		return err
 	}
-
 	return nil
 }
 
 func createHeader(payloadSize int) []byte {
-	header := make([]byte, 0)
-	//TODO: poner en cte
-	header = append(header, '0') //tipo mensaje = enviar bet
+	header := make([]byte, 0) //TODO: poner 0 en cte
+
+	typeMessageBytes := make([]byte, AMOUNT_BYTES_UINT8)
+	typeMessageBytes[0] = byte('0')
+	header = append(header, typeMessageBytes...)
 
 	payloadSizeBytes := make([]byte, AMOUNT_BYTES_UINT32)
 	binary.BigEndian.PutUint32(payloadSizeBytes, uint32(payloadSize))
@@ -42,7 +46,19 @@ func createHeader(payloadSize int) []byte {
 }
 
 // avisa que no se van a mandar mas apuestas
-func SendEnd(socket io.Writer) error { return nil }
+func SendEnd(socket io.Writer) error {
+	messageEnd := make([]byte, 0) //TODO: poner 0 en cte
+
+	typeMessageBytes := make([]byte, AMOUNT_BYTES_UINT8)
+	typeMessageBytes[0] = byte('1')
+	messageEnd = append(messageEnd, typeMessageBytes...)
+
+	if err := safe_socket.SendAll(socket, messageEnd); err != nil {
+		logger.Error("send-message", logger.Fail) //TODO: ver que mas agregarle al log y si es correcto ponerlo aca o con la capa de arriba basta xd
+		return err
+	}
+	return nil
+}
 
 // espera de manera bloqueante a q llegue el mensaje del ganador y lo deserializa devolviendole al cliente una Bet
 func ReceiveWinners(socket io.Reader) ([]model.Bet, error) { return []model.Bet{}, nil }
