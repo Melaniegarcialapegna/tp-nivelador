779ce4e361119f6427b98d1a9f6d8acaf1621512
Melaniegarcialapegna <mgarcial@fi.uba.ar>
2026-08-31 22:10:18 -0300

    Modularizar serializacion de bets del lado del servidor.

--- services/server/src/protocol/serialization.py
+++ services/server/src/protocol/serialization.py
@@ -1,35 +1,42 @@
-
 from lottery import Bet 
 
-AMOUNT_BYTES_UINT32 = 4 #TODO : cambiar xd
-AMOUNT_BYTES_UINT16 = 2 #TODO : cambiar xd
+FIXED_FIELDS_SIZE_BYTES = 4 
 BIRTHDATE_LENGTH = 10 #YYYY-MM-DD
-AMOUNT_BYTES_CONST = AMOUNT_BYTES_UINT32 + AMOUNT_BYTES_UINT32 + AMOUNT_BYTES_UINT32 + BIRTHDATE_LENGTH
+
+DIYNAMIC_FIELD_LENGHT_SIZE_BYTES = 2 
+
+AMOUNT_BYTES_CONST = FIXED_FIELDS_SIZE_BYTES * 3 + BIRTHDATE_LENGTH  # agency_id + document + number + birthdate
 
 # Serializes a bet to a byte array
-# For the fields that are dinamic in size, will be used a separator to know how many bytes to read for each field
-# like long_dinamic_field_i|dinamic_field_i|
+# For the fields that are dinamic in size, will be used a separator to know how many bytes to read for each field 
+# long_dinamic_field_i | dinamic_field_i |
 def serialize_bet(bet: Bet) -> bytes:
-    #pongo los campos fijos
-    bet_bytes = bet.agency_id.to_bytes(AMOUNT_BYTES_UINT32, byteorder='big')
-    bet_bytes += bet.document.to_bytes(AMOUNT_BYTES_UINT32, byteorder='big')
-    bet_bytes += bet.number.to_bytes(AMOUNT_BYTES_UINT32, byteorder='big')
+    #put fixed fields 
+    bet_bytes = _get_field_bytes(bet.agency_id)
+    bet_bytes += _get_field_bytes(bet.document)
+    bet_bytes += _get_field_bytes(bet.number)
     bet_bytes += str(bet.birthdate).encode('utf-8')
 
-    #pongo los campos dinmicos
-    bet_bytes += get_dynamic_field(bet.first_name)
-    bet_bytes += get_dynamic_field(bet.last_name)
+    #put dinamic fields
+    bet_bytes += _get_dynamic_field(bet.first_name)
+    bet_bytes += _get_dynamic_field(bet.last_name)
 
     return bet_bytes
 
-def get_dynamic_field(field: str) -> bytes:
-    #campos en bytes
-    field_bytes = field.encode('utf-8')
-    #longitud campo
-    field_length = len(field_bytes)
-    field_length_bytes = field_length.to_bytes(AMOUNT_BYTES_UINT16, byteorder='big')
+def _get_field_bytes(field) -> bytes:
+    return field.to_bytes(FIXED_FIELDS_SIZE_BYTES, byteorder='big')
 
-    return field_length_bytes + field_bytes
+def _get_dynamic_field(field: str) -> bytes:
+    #convert field to bytes
+    field_bytes = field.encode('utf-8')
+
+    #length of the field in bytes
+    field_length = len(field_bytes)
+    field_length_bytes = field_length.to_bytes(DIYNAMIC_FIELD_LENGHT_SIZE_BYTES, byteorder='big')
+
+    return field_length_bytes + field_bytes #header of the bet
+
+#--
 
 #Deserializes a byte array to a bet
 def deserialize_bet(bet_bytes: bytes) -> Bet:
@@ -38,23 +45,17 @@ def deserialize_bet(bet_bytes: bytes) -> Bet:
 
     position = 0
 
-    #TODO: modularizar
-    agency_id = int.from_bytes(bet_bytes[position:position + AMOUNT_BYTES_UINT32], byteorder='big')
-    position += AMOUNT_BYTES_UINT32
 
-    document = int.from_bytes(bet_bytes[position:position + AMOUNT_BYTES_UINT32], byteorder='big')
-    position += AMOUNT_BYTES_UINT32
 
-    number = int.from_bytes(bet_bytes[position:position + AMOUNT_BYTES_UINT32], byteorder='big')
-    position += AMOUNT_BYTES_UINT32
+    agency_id , position = _get_int_from_field(bet_bytes, position, FIXED_FIELDS_SIZE_BYTES)
+    document , position = _get_int_from_field(bet_bytes, position, FIXED_FIELDS_SIZE_BYTES)
+    number , position = _get_int_from_field(bet_bytes, position, FIXED_FIELDS_SIZE_BYTES)
 
-    birthdate = bet_bytes[position:position + BIRTHDATE_LENGTH].decode('utf-8')
-    position += BIRTHDATE_LENGTH
+    birthdate , position = _get_str_from_field(bet_bytes, position, BIRTHDATE_LENGTH)
 
-    first_name, newPosition = read_dynamic_field(bet_bytes, position)
-    position = newPosition
+    first_name, position = _read_dynamic_field(bet_bytes, position)
 
-    last_name, newPosition = read_dynamic_field(bet_bytes, position)
+    last_name, _ = _read_dynamic_field(bet_bytes, position)
 
     return Bet(
         agency_id=agency_id,
@@ -64,19 +65,22 @@ def deserialize_bet(bet_bytes: bytes) -> Bet:
         birthdate=birthdate,
         number=number)
 
-
-def read_dynamic_field(bet_bytes: bytes, position: int) -> (str, int):
-    if position + AMOUNT_BYTES_UINT16 > len(bet_bytes):
+def _read_dynamic_field(bet_bytes: bytes, position: int):
+    if position + DIYNAMIC_FIELD_LENGHT_SIZE_BYTES > len(bet_bytes):
         raise ValueError("Data is too short to contain field length")
 
-    length = int.from_bytes(bet_bytes[position:position + AMOUNT_BYTES_UINT16], byteorder='big')
-    position += AMOUNT_BYTES_UINT16
+    field_length , position = _get_int_from_field(bet_bytes, position, DIYNAMIC_FIELD_LENGHT_SIZE_BYTES)
 
-    if position + length > len(bet_bytes):
+    if position + field_length > len(bet_bytes):
         raise ValueError("Data is too short to contain the dynamic field")
 
-    field = bet_bytes[position:position + length].decode('utf-8')
-    position += length
+    field , position = _get_str_from_field(bet_bytes, position, field_length)
 
     return field, position
-	
+
+
+def _get_int_from_field(bet_bytes: bytes, position: int, length: int):
+    return int.from_bytes(bet_bytes[position:position + length], byteorder='big'), position + length
+
+def _get_str_from_field(bet_bytes: bytes, position: int, length: int):
+    return bet_bytes[position:position + length].decode('utf-8') , position + length
