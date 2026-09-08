24c30ad8f05ca8b702f32bb732ace7bca29cc789
Melaniegarcialapegna <mgarcial@fi.uba.ar>
2026-09-07 22:12:13 -0300

    fix en sockets

--- services/client/src/safe_socket/safe_socket.go
+++ services/client/src/safe_socket/safe_socket.go
@@ -1,7 +1,6 @@
 package safe_socket
 
 import (
-	"errors"
 	"io"
 )
 
@@ -11,9 +10,6 @@ func SendAll(socket io.Writer, bytes []byte) error {
 		sizeSent, err := socket.Write(bytes[totalSent:])
 		if err != nil {
 			return err
-		}
-		if sizeSent == 0 {
-			return errors.New("connection closed before sending all data")
 		}
 		totalSent += sizeSent
 	}
@@ -27,9 +23,6 @@ func RecvAll(socket io.Reader, size int) ([]byte, error) {
 		sizeRead, err := socket.Read(buff[totalRead:])
 		if err != nil {
 			return nil, err
-		}
-		if sizeRead == 0 {
-			return nil, errors.New("connection closed before reading all data")
 		}
 		totalRead += sizeRead
 	}
--- services/server/src/safe_socket/safe_socket.py
+++ services/server/src/safe_socket/safe_socket.py
@@ -5,8 +5,6 @@ def send_all(socket: socket.socket, bytes):
     total_sent = 0
     while total_sent < len(bytes): #loop continue until all bytes are sent
         size_sent = socket.send(bytes[total_sent:])
-        if size_sent == 0: 
-            raise ConnectionError("Connection closed before sending all data")
         total_sent += size_sent
     return total_sent
 
@@ -14,7 +12,5 @@ def recv_all(socket: socket.socket, size):
     data = b""
     while len(data) < size: #loop continue until all bytes are read
         data_read = socket.recv(size-len(data))
-        if data_read == b"": 
-            raise ConnectionError("Connection closed before reading all data")
         data += data_read
     return data
